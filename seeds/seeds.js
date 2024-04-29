bc1bcb562bdb00d403bb4b8ddecd3fd00ea40c8c
runowo96 <devinmrunowski@gmail.com>
2024-04-29 16:50:08 -0400

    connect seed data

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,13 +1,6 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
+const usersData = require('../seed-data/users');
+
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex('user').del();
+  await knex('user').insert(usersData);
 };
